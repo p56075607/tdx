6fc56675b440dd68c0e49cbdf7a3ef3514427efa
p56075607 <p56075607@gmail.com>
2022-05-16 21:52:08 +0800

    solve memory

--- include_geojson.php
+++ include_geojson.php
@@ -1,9 +1,10 @@
 <?php
+ini_set('memory_limit', '1G');
+
 # 加入村里界圖的 geojson
 $json = json_decode(file_get_contents(__DIR__ . '/raw/map/20210324.json'), true);
-
-
-
+#print_r($json);
+exit();
 
 $count = [];
 foreach (glob(__DIR__ . '/raw/*.csv') as $csvfile) {
